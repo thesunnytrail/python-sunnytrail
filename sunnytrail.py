ba2b0583e09ac27c462e1eb299bc3ee2c7f19718
Andrei Savu <savu.andrei@gmail.com>
2010-10-01 16:41:48 +0300

    added dummy classes

--- sunnytrail.py
+++ sunnytrail.py
@@ -1,9 +1,37 @@
 """ Sunnytrail client library.
 
-The easiest and fastest way to integrate. This file can also be used as CLI 
-tool if you wish to play with the API."""
+The easiest and fastest way to integrate. This file can also be used as CLI tool if you wish to play with the API."""
+
+import sys
+import urllib
+import simplejson
+
+class SunnytrailException(Exception): 
+  """ Generic class for Sunnytrail related exceptions """
+  pass
 
 class Sunnytrail(object):
+  def __init__(self, key, base_url='api.thesunnytrail.com', use_ssl=True):
+    self.key = key
+    self._base_url = ("https://%s" if use_ssl else "http://%s") % url
+    self._use_ssl = use_ssl
+
+  def send_event(self, event):
+    """ Send an event to the API """
+    print event.to_json()
+    
+class Event(object):
+  def to_json(self):
+    pass
+
+class SignupEvent(Event):
+  def __init__(self, name, email, plan):
+    pass
+
+class PayEvent(Event):
+  pass
+
+class CancelEvent(Event):
   pass
 
 def main():
--- test.py
+++ test.py
@@ -4,6 +4,11 @@ import mox
 import unittest
 import sunnytrail
 
+class EventTest(unittest.TestCase):
+
+  def test_create_signup_event(self):
+    e = sunnytrail.SignupEvent()
+
 class ApiTestCase(unittest.TestCase):
   def setUp(self):
     self.mox = mox.Mox()
