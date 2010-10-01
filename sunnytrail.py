da9fa9d531471a685aed3118a09bc4966eb9c22d
Andrei Savu <savu.andrei@gmail.com>
2010-10-01 12:14:56 +0300

    initial import

---
+++ sunnytrail.py
@@ -0,0 +1,14 @@
+""" Sunnytrail client library.
+
+The easiest and fastest way to integrate. This file can also be used as CLI 
+tool if you wish to play with the API."""
+
+class Sunnytrail(object):
+  pass
+
+def main():
+  pass
+
+if __name__ == '__main__':
+  sys.exit(main())
+
---
+++ test.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+import unittest
+import sunnytrail
+
+class ApiTestCase(unittest.TestCase):
+  pass
+
+class SunnyTrailTest(ApiTestCase:
+  pass
+
+if __name__ == '__main__':
+  unittest.main()
+
