58ed96b58708f679eff3a06230667199f16f0140
Andrei Savu <savu.andrei@gmail.com>
2010-10-07 17:44:01 +0300

    basic wrapper setup scripts

--- setup.py
+++ setup.py
@@ -1,3 +1,22 @@
 
-# TBD - install script for pypi & friends
+from distutils.core import setup
+
+try:
+  import setuptools
+except ImportError:
+  pass
+
+setup(
+  name='python-sunnytrail',
+  version='0.1',
+  description='Python Wrapper for the Sunnytrail API',
+  author='Sunnytrail Team',
+  url='http://www.thesunnytrail.com/',
+  py_modules=['sunnytrail'],
+  classifiers=[
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Developers',
+    'Programming Language :: Python',
+  ],
+)
 
